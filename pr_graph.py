e0a61b629507ebd13363e8271d0bd4e062e0962d
Brian Bockelman <bbockelm@cse.unl.edu>
2009-11-25 04:29:35 +0000

    Started work on the transfer graph; added atomic file updates and SIGALRM.
    git-svn-id: https://vdt.cs.wisc.edu/svn/software/osg-display-data/osg_display@18394 4e558342-562e-0410-864c-e07659590f8c

--- pr_graph.py
+++ pr_graph.py
@@ -3,8 +3,11 @@ import os
 import sys
 import sets
 import time
+import shutil
+import signal
 import urllib2
 import datetime
+import tempfile
 import optparse
 import ConfigParser
 
@@ -29,8 +32,8 @@ class OIMDataSource(object):
 
     resource_group_url = 'http://myosg.grid.iu.edu/rgsummary/xml?datasource=' \
         'summary&all_resources=on&gridtype=on&gridtype_1=on&active=on&' \
-        'active_value=1&disable=on&disable_value=0&summary_attrs_showhierarchy=on' \
-        '&summary_attrs_showservice=on'
+        'active_value=1&disable=on&disable_value=0&' \
+        'summary_attrs_showhierarchy=on&summary_attrs_showservice=on'
 
     def query_sites(self):
         fd = urllib2.urlopen(self.resource_group_url)
@@ -158,14 +161,117 @@ class DataSource(object):
         ) as foo
         """
 
-class PRGraph(object):
+
+class TransferData(object):
+
+    def __init__(self):
+        self.time = None
+        self.count = None
+        self.volume_mb = None
+
+
+def get_files(cp, name):
+    name = cp.get("Filenames", name)
+    fd, tmpname = tempfile.mkstemp(prefix="osg_display")
+    os.close(fd)
+    return name, tmpname
+
+def commit_files(name, tmpname):
+    shutil.move(tmpname, name)
+
+
+class DataSourceTransfers(object):
 
     def __init__(self, cp):
         self.cp = cp
+        self.data = {}
+        
+    def run(self):
+        self.connect()
+        self.load_cached()
+        self.determine_missing()
+        
+    def connect(self):
+        user = self.cp.get("Gratia", "User")
+        password = self.cp.get("Gratia", "Password")
+        host = self.cp.get("Gratia", "Host")
+        database = self.cp.get("Gratia", "Database")
+        port = int(self.cp.get("Gratia", "Port"))
+        self.conn = MySQLdb.connect(user=user, passwd=password, host=host,
+            port=port, db=database)
+
+    def load_cached(self):
+        try:
+            data = pickle.load(open(self.cp.get("Filenames", "transfers_data"),
+                "r"))
+            # Verify we didn't get useless data
+            for time, tdata in data.items():
+                assert isinstance(time, datetime.datetime)
+                assert isinstance(tdata, TransferData)
+                assert isinstance(tdata.time, datetime.datetime)
+                assert tdata.count != None
+                assert tdata.volume_mb != None
+            self.data = data
+        except:
+            pass
+
+    def save_cache(self):
+        now = datetime.datetime.now()
+        old_keys = []
+        for key in self.data.keys():
+            if (now - key).days >= 2:
+                old_keys.append(key)
+        for key in old_keys:
+            del self.data
+        try:
+            name, tmpname = get_files(self.cp, "transfers_data")
+            fp = open(tmpname, 'w')
+            pickle.dump(self.data, fp)
+            fp.close()
+            commit_files(name, tmpname)
+        except:
+            pass
+
+    def get_params(self):
+        hours = int(self.cp.get("Gratia", "hours"))
+        now = int(time.time()-60)
+        prev = now - 3600*hours
+        offset = prev % 3600
+        starttime = datetime.datetime(*time.gmtime(prev)[:6])
+        endtime = datetime.datetime(*time.gmtime(now)[:6])
+        return {'offset': offset, 'starttime': starttime, 'endtime': endtime,
+            'span': 3600}
+
+    def query_users(self):
+        curs = self.conn.cursor()
+        params = self.get_params()
+        curs.execute(self.users_query, params)
+        return [i[1] for i in curs.fetchall()]
+
+    transfers_query = """
+        SELECT
+          (truncate((unix_timestamp(ServerDate))/%(span)s, 0)*%(span)s) as time,
+          sum(Value*SU.Multiplier) as Records
+        FROM JobUsageRecord_Meta JURM
+        JOIN JobUsageRecord JUR on JURM.dbid=JUR.dbid
+        JOIN Network N on (JUR.dbid = N.dbid)
+        JOIN SizeUnits SU on N.StorageUnit = SU.Unit
+        WHERE
+          ResourceType="Storage" AND
+          ServerDate >= %(starttime)s AND
+          ServerDate < %(endtime)s
+        GROUP BY time;
+        """
+
+
+class PRGraph(object):
+
+    def __init__(self, cp, graph_num):
+        self.cp = cp
+        self.num = graph_num
 
     def build_canvas(self):
-        ylabel1 = self.cp.get("Labels", "YLabel1")
-        ylabel2 = self.cp.get("Labels", "YLabel2")
+        ylabel = self.cp.get("Labels", "YLabel%i" % self.num)
         if False:
             FigureCanvas = matplotlib.backends.backend_svg.FigureCanvasSVG
         else:
@@ -177,32 +283,25 @@ class PRGraph(object):
         height_inches = height/float(dpi)
         width = int(self.cp.get("Sizes", "width"))
         width_inches = width/float(dpi)
+        self.height, self.width = height, width
         fig.set_size_inches(width_inches, height_inches)
         fig.set_dpi(dpi)
         fig.set_facecolor('white')
-        ax_rect = (.11, 0.06, .78, .85)
-        #ax = HostAxes(fig, ax_rect)
-        #ax2 = ParasiteAxes(ax, sharex=ax)
+        ax_rect = (.11, 0.06, .89, .85)
         ax = fig.add_axes(ax_rect)
-        ax2 = ax.twinx()
         frame = ax.patch
         frame.set_fill(False)
         ax.grid(True, color='#555555', linewidth=1)
 
         ax.set_frame_on(False)
         ax.get_xaxis().set_visible(False)
-        ylabel = ax.set_ylabel(ylabel1)
-        ylabel.set_size(int(self.cp.get("Sizes", "YLabelSize")))
-        ylabel = ax2.set_ylabel(ylabel2)
+        ylabel = ax.set_ylabel(ylabel)
         ylabel.set_size(int(self.cp.get("Sizes", "YLabelSize")))
         setp(ax.get_yticklabels(), size=int(self.cp.get("Sizes", "YTickSize")))
-        setp(ax2.get_yticklabels(), size=int(self.cp.get("Sizes", "YTickSize")))
         setp(ax.get_ygridlines(), linestyle='-')
         setp(ax.get_yticklines(), visible=False)
-        setp(ax2.get_yticklines(), visible=False)
 
-        self.ax1 = ax
-        self.ax2 = ax2
+        self.ax = ax
         self.canvas = canvas
         self.fig = fig
 
@@ -210,9 +309,9 @@ class PRGraph(object):
         self.canvas.draw()
         if False:
             renderer = matplotlib.backends.backend_svg.RendererSVG( \
-                width, height, self.file)
+                self.width, self.height, self.file)
             self.canvas.figure.draw(renderer)
-            renderer.finish()
+            renderer.finalize()
         else:
             size = self.canvas.get_renderer().get_canvas_width_height()
             buf = self.canvas.tostring_argb()
@@ -223,32 +322,20 @@ class PRGraph(object):
             im.save(self.file, format="PNG")
 
     def draw(self):
-        data_len = len(self.data1)
+        data_len = len(self.data)
         X = range(data_len)
-        color1 = self.cp.get("Colors", "Line1")
-        color2 = self.cp.get("Colors", "Line2")
-        line1 = self.ax1.plot(X, self.data1, marker="o", markeredgecolor=color1,
+        color = self.cp.get("Colors", "Line%i" % self.num)
+        line = self.ax.plot(X, self.data, marker="o", markeredgecolor=color,
             markerfacecolor="white", markersize=13, linewidth=5,
-            markeredgewidth=5, color=color1, label=self.cp.get("Labels", "Legend1"))
-        line2 = self.ax2.plot(X, self.data2, marker="o", markeredgecolor=color2,
-            markerfacecolor="white", markersize=13, linewidth=5,
-            markeredgewidth=5, color=color2, label=self.cp.get("Labels", "Legend2"))
-        max_ax1 = max(self.data1)*1.1
-        max_ax2 = max(self.data2)*1.1
-        self.ax1.set_ylim(-0.5, max_ax1)
-        self.ax1.set_xlim(-1, data_len)
-        ax1_ticks = self.ax1.get_yticks()
-        ax2_ticks = [int(i/max_ax1*max_ax2) for i in ax1_ticks]
-        self.ax2.set_yticks(ax2_ticks)
-        self.ax2.set_ylim(-0.5, max_ax2)
-        legend1 = self.ax1.legend(loc=2, mode="expand",
-            bbox_to_anchor=(0., 1.02, 1., .102))
-        legend2 = self.ax2.legend(loc=1, mode="expand",
-            bbox_to_anchor=(0.5, 1.02, 0.5, .102))
-        setp(legend1.get_frame(), visible=False)
-        setp(legend2.get_frame(), visible=False)
-        setp(legend1.get_texts(), size=int(self.cp.get("Sizes", "LegendSize")))
-        setp(legend2.get_texts(), size=int(self.cp.get("Sizes", "LegendSize")))
+            markeredgewidth=5, color=color, label=self.cp.get("Labels",
+            "Legend%i" % self.num))
+        max_ax = max(self.data)*1.1
+        self.ax.set_ylim(-0.5, max_ax)
+        self.ax.set_xlim(-1, data_len)
+        legend = self.ax.legend(loc=9, mode="expand",
+            bbox_to_anchor=(0.5, 1.02, 1., .102))
+        setp(legend.get_frame(), visible=False)
+        setp(legend.get_texts(), size=int(self.cp.get("Sizes", "LegendSize")))
 
     def parse_data(self):
         pass
@@ -259,6 +346,7 @@ class PRGraph(object):
         self.build_canvas()
         self.draw()
         self.write_graph()
+        fp.close()
 
 
 class PRData(object):
@@ -296,66 +384,64 @@ class PRData(object):
         info['num_ses'] = int(self.num_ses)
         fp.write(str(info))
         fp.flush()
+        fp.close()
 
 
 def configure():
-    usage = "usage: %prog [options] graph_out_filename data_out_filename"
+    usage = "usage: %prog -c config_file"
     parser = optparse.OptionParser()
     parser.add_option("-c", "--config", help="PR Graph config file",
         dest="config")
     opts, args = parser.parse_args()
 
-    if not args:
-        parser.print_help()
-        print "\nMust specify a output filename (or '-' for stdout)"
-        sys.exit(1)
-
-    filename = args[0]
-    if filename != '-':
-        fp = open(filename, 'w')
-    else:
-        fp = sys.stdout
-
-    if len(args) < 2:
-        parser.print_help()
-        print "\nMust specify an output filename for the data files (or '-' for " \
-            "stdout)"
-        sys.exit(1)
-    filename2 = args[1]
-    if filename != '-':
-        fp2 = open(filename2, 'w')
-    else:
-        fp2 = sys.stdout
-
     if not opts.config:
         parser.print_help()
         print "\nMust pass a config file."
+        sys.exit(1)
 
     cp = ConfigParser.ConfigParser()
     cp.readfp(open(opts.config, "r"))
-    return cp, fp, fp2
+    return cp
 
 def main():
-    cp, fp, fp2 = configure()
+    cp = configure()
+
+    # Set the alarm in case if we go over time
+    signal.alarm(int(cp.get("Settings", "timeout")))
 
     # Generate the graphs
-    pr = PRGraph(cp)
     ds = DataSource(cp)
     ds.run()
-    pr.data1 = ds.query_jobs()
-    pr.data2 = ds.query_users()
-    pr.run(fp)
+    pr = PRGraph(cp, 1)
+    pr.data = ds.query_jobs()
+    num_jobs = sum(pr.data)*60
+
+    name, tmpname = get_files(cp, "jobs")
+    fd = open(tmpname, 'w')
+    pr.run(fd)
+    commit_files(name, tmpname)
+
+    pr = PRGraph(cp, 2)
+    pr.data = ds.query_users()
+    name, tmpname = get_files(cp, "users")
+    fd = open(tmpname, 'w')
+    pr.run(fd)
+    commit_files(name, tmpname)
 
     # Generate the JSON
     ods = OIMDataSource(cp)
     prd = PRData(cp)
     prd.set_num_sites(len(ods.query_sites()))
-    prd.set_num_jobs(sum(pr.data1)*60)
+    prd.set_num_jobs(num_jobs)
     prd.set_num_users(ds.query_user_num())
     ces, ses = ods.query_ce_se()
     prd.set_num_ces(ces)
     prd.set_num_ses(ses)
-    prd.run(fp2)
+
+    name, tmpname = get_files(cp, "json")
+    fd = open(tmpname, 'w')
+    prd.run(fd)
+    commit_files(name, tmpname)
 
 if __name__ == '__main__':
     main()
